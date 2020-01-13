8ab8b5e0843829f3fc42b3502b4814ec3c7baa63
Joseph Kim <joseph97@vt.edu>
2020-01-13 12:28:03 -0500

    reactive assignment

--- angular/assignments/forms-reactive-start/src/app/app.component.ts
+++ angular/assignments/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,31 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], 
+          this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // ) 
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+    ) 
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@gmail.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    })
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Bob'
+      }
+    })
   }
 
   onAddHobby() {
@@ -36,6 +57,19 @@ export class AppComponent implements OnInit {
       return {'nameIsForbidden': true};
     }
     return null; // must return something or null, NOT {'s': false}
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+    return promise;
   }
 
   onSubmit() {
