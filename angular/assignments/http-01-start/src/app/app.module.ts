b0f5f8aa8a607d5c1584137067bc9e1c5b4745f8
Joseph Kim <joseph97@vt.edu>
2020-01-23 10:43:31 -0500

    finish pipes

--- angular/assignments/http-01-start/src/app/app.module.ts
+++ angular/assignments/http-01-start/src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ReversePipe } from './reverse.pipe';
-import { SortPipe } from './sort.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReversePipe,
-    SortPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
